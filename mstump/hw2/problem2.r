d78c9d00d7c3c26fa6f37a8b9f2f4155d38c9fb2
Matt Stump <mrevilgnome@gmail.com>
2011-01-29 12:19:15 -0800

    Added data for problem 1 and 2 and supporting source file for problem 2.

---
+++ mstump/hw2/problem2.r
@@ -0,0 +1,25 @@
+require("rpart")
+
+# train the filter
+training_data <- read.csv('data/table4_8pg199.txt', header=TRUE)
+training_data[2] <- as.factor(training_data[,2])
+training_data[3] <- as.factor(training_data[,2])
+target <- as.factor(training_data[,5])
+
+tree <- rpart(target~., data=training_data[2:4], control=rpart.control(minsplit=0, minbucket=0, cp=-1, maxcompete=0, maxsurrogate=0, usesurrogate=0, xval=0, maxdepth=3))
+
+# plot the tree
+plot(tree)
+text(tree)
+
+predict_data <- read.csv('data/problem2_data.csv', header=TRUE)
+predict_data[2] <- as.factor(predict_data[,2])
+predict_data[3] <- as.factor(predict_data[,2])
+
+# str(predict_data)
+# str(predict_data[2:4])
+
+prediction <- predict(tree, predict_data[2:4], type="class")
+str(prediction)
+
+# 1 - sum(target==prediction) / length(predict_data)
