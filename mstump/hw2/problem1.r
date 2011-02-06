47dc88a79b87698f1e93519534219ba193a5d24b
Matt Stump <mrevilgnome@gmail.com>
2011-02-05 23:35:29 -0800

    Finished hw2 problem 1

--- mstump/hw2/problem1.r
+++ mstump/hw2/problem1.r
@@ -1,8 +1,11 @@
 require("reldist")
 
 d <- read.csv('data/table4_8pg199.txt', header=TRUE)
+cat("Data:\n")
+print(d)
+
 sorted <- d[order(d[4]), ]
-print(sorted)
+scount <- nrow(sorted)
 
 findsplits <- function(h, t, r) {
     if (length(t) > 0) {
@@ -26,21 +29,66 @@ cerror <- function(df) {
     return(1 - max(e1, e2))
 }
 
-split <- findsplits(sorted$a3[1], tail(sorted$a3, -1), vector())
+centropy <- function(df) {
+    e1 <- nrow(subset(df, Target == 1)) / nrow(df)
+    e2 <- nrow(subset(df, Target == 0)) / nrow(df)
+    t <- ((-1 * e1) * log2(e1) - (e2 * log2(e2)))
+    if (is.nan(t))
+      return(0)
+
+    return(t)
+}
+
+cgini <- function(df) {
+    e1 <- nrow(subset(df, Target == 1)) / nrow(df)
+    e2 <- nrow(subset(df, Target == 0)) / nrow(df)
+    return(1 - e1^2 - e2^2)
+}
 
 error <- data.frame()
+gini <- data.frame()
+entropy <- data.frame()
+pentropy <- centropy(sorted)
+split <- findsplits(sorted$a3[1], tail(sorted$a3, -1), vector())
+
 for (s in split) {
+
+    ## split the dataframe at the specified pt
     le <- subset(sorted, a3 <= s)
     gt <- subset(sorted, a3 >  s)
 
-    lr <- nrow(le) / nrow(sorted)
-    gr <- nrow(gt) / nrow(sorted)
+    lecount <- nrow(le)
+    gtcount <- nrow(gt)
+
+    ## classification error
+    lr <- lecount / scount
+    gr <- gtcount / scount
 
     leerr <- cerror(le)
     gterr <- cerror(gt)
 
     err <- (lr * leerr) + (gr * gterr)
-
     error <- rbind(error, data.frame("split"=s, "error"=err, "le err"=leerr, "gt err"=gterr))
+
+    ## entropy
+    lentropy <- centropy(le)
+    gentropy <- centropy(gt)
+    gain     <- pentropy - (((lentropy * lecount) / scount) + ((gentropy * gtcount) / scount))
+    entropy  <- rbind(entropy, data.frame("split"=s, "le"=lentropy, "gt"=gentropy, "gain"=gain))
+
+    ## gini
+    lgini <- cgini(le)
+    ggini <- cgini(gt)
+    sgini <- ((lecount/scount) * lgini) + ((gtcount/scount) * ggini)
+    gini  <- rbind(gini, data.frame("split"=s, "le"=lgini, "gt"=ggini, "gini"=sgini))
+
 }
+cat("\nMisclassification error:\n")
 print(error)
+
+cat("\nParent entropy:  ", pentropy, "\n")
+cat("\nSplit entropy:\n")
+print(entropy)
+
+cat("\nGini:\n")
+print(gini)
