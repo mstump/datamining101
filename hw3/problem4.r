4624bba91d103789ab8bf800e5526eb53836cbe2
Matt Stump <mrevilgnome@gmail.com>
2011-02-12 02:27:46 -0800

    add problem 4 graph

--- hw3/problem4.r
+++ hw3/problem4.r
@@ -1,14 +1,5 @@
 library("MASS")
 data    <- read.csv('data/winequality-red.txt', sep=";", header=TRUE)
 
-coef <- data.frame()
-for (i in 1:20) {
-    exp <- (3 - 4 * (i / 20))
-	lam <- 10 ^ exp
-    rr <- lm.ridge(data$quality~., data=data[,1:12], lambda=lam)
-    coef <- rbind(coef, lm.wine$coef)
-    str(lm.wine$coef)
-}
-
-str(coef)
-print(coef)
+rr <- lm.ridge(data$quality~., data=data[,1:12], lambda=seq(1, 10000, 100))
+matplot(rr$lambda, t(rr$coef/rr$scales), type="l")
