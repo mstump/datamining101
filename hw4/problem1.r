06edf1b868caeabcb44c6f10395f95e4824a50e2
Matt Stump <mrevilgnome@gmail.com>
2011-02-13 01:33:06 -0800

    hw4 problem 1: ggplot intead of plot and test error

--- hw4/problem1.r
+++ hw4/problem1.r
@@ -1,5 +1,6 @@
-library("class")
+#library("class")
 library("MASS")
+library("ggplot2")
 
 data       <- read.csv('data/winequality-red.txt', sep=";", header=TRUE)
 data.size  <- nrow(data)
@@ -9,11 +10,24 @@ test.size  <- data.size - train.size
 train.data <- head(data, train.size)
 test.data  <- tail(data, test.size)
 
-err <- function (x) {
+ef.train <- function (x) {
     1 - sum(train.data$quality == round(x)) / train.size
 }
 
-model <- lm.ridge(train.data$quality~., data=train.data[,1:11], lambda=exp(seq(-15,10,by=0.01)))
-preds <- as.matrix(train.data[,1:11]) %*% t(coef(model)[,-1]) +  rep(1,train.size) %o% coef(model)[,1]
-train.err <- data.frame(apply(preds, 2, err))
-plot(as.numeric(row.names(train.err)), train.err[,1], type='l')
+ef.test <- function (x) {
+    1 - sum(test.data$quality == round(x)) / test.size
+}
+
+model <- lm.ridge(train.data$quality~., data=train.data[,1:11], lambda=exp(seq(-15,6,by=0.01)))
+
+train.pred <- as.matrix(train.data[,1:11]) %*% t(coef(model)[,-1]) +  rep(1,train.size) %o% coef(model)[,1]
+train.err  <- data.frame("err"=apply(train.pred, 2, ef.train))
+train.err  <- cbind(train.err, "lambda"=as.numeric(row.names(train.err)))
+
+test.pred <- as.matrix(test.data[,1:11]) %*% t(coef(model)[,-1]) +  rep(1,test.size) %o% coef(model)[,1]
+test.err  <- data.frame("err"=apply(test.pred, 2, ef.test))
+test.err  <- cbind(test.err, "lambda"=as.numeric(row.names(test.err)))
+
+
+p <- qplot(lambda, err, data=train.err, geom="line", colour=I("red"))
+p + geom_line(mapping=aes(x=lambda, y=err), data=test.err, colour=I("blue"))
